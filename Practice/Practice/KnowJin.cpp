422514835eca4be16f513ee4181de3e78c6b3759
roomMaker <leehuk0622@gmail.com>
2022-06-15 17:38:45 +0900

    Update KnowJin.cpp

--- Practice/Practice/KnowJin.cpp
+++ Practice/Practice/KnowJin.cpp
@@ -2,32 +2,26 @@
 #include <Windows.h>
 void SeeKnowJin();
 
-
+int knowTalk = 0;
 int main()
 {
-	int count = 0;
 	srand(time(NULL));
 	while (1) 
 	{
 		SeeKnowJin();
-		if (count == 20)
-		{
-			system("cls");
-			count = 0;
-		}
-		count++;
 	}
 	return 0;
 }
 
 void SeeKnowJin()
 {
-	int knowTalk = 0;
-	knowTalk = 1 + rand() % 6;
+	
+	knowTalk++;
+	if (knowTalk == 7) knowTalk = 1;
 	switch (knowTalk)
 	{
 	case 1:
-		std::cout << "허벅지가 참 이쁘네..." << std::endl;
+		std::cout << "\"노우진식 테크톡\"을 시작해볼까?" << std::endl;
 		break;
 	case 2:
 		std::cout << "\"노우진식 기립팔짱수면\"" << std::endl;
@@ -36,15 +30,17 @@ void SeeKnowJin()
 		std::cout << "오늘은 어느 쪽 허벅지를 볼까?" << std::endl;
 		break;
 	case 4:
-		std::cout << "\"노우진식 테크톡\"을 시작해볼까?" << std::endl;
+		std::cout << "허벅지가 참 이쁘네..." << std::endl;
 		break;
 	case 5:
-		std::cout << "쥬라기공원? 좀 별로였어" << std::endl;
-		break;
-	case 6:
 		std::cout << "\"노우진식 간접참조\"" << std::endl;
 		break;
+	case 6:
+		std::cout << "쥬라기공원? 좀 별로였어" << std::endl;
+		Sleep(1000);
+		system("cls");
+		break;
 	}
-		Sleep(rand() % 1000);
+		Sleep(1000);
 	
 }
