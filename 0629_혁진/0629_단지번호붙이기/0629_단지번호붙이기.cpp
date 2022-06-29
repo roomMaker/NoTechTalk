3239a7033593c1919d5ab1294a43b002d3f4ce0f
shinshinjin <sinhukjin01@naver.com>
2022-06-29 17:51:42 +0900

    0629_트리_백준_단지번호
    0629_트리_백준_단지번호_SHJ

---
+++ 0629_혁진/0629_단지번호붙이기/0629_단지번호붙이기.cpp
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+int map[25][25];
+bool isVisited[25][25];
+
+int dfs(int r, int c)
+{
+	// 집에 방문 했을 때 => 집의 개수를 하나 세는 것
+	isVisited[r][c] = true;
+
+	// lookup table
+	static const int dr[] = { -1, 1, 0, 0 };
+	static const int dc[] = { 0, 0, -1, 1 };
+
+	int houseCount = 1;
+	for (int i = 0; i < 4; ++i)
+	{
+		int nr = r + dr[i];
+		int nc = c + dc[i];
+
+		if (nr < 0 || nr >= N || nc < 0 || nc >= N)
+		{
+			continue;
+		}
+		if (isVisited[nr][nc] || map[nr][nc] == 0)
+		{
+			continue;
+		}
+		houseCount += dfs(nr, nc);
+	}
+	return houseCount;
+}
+
+int main()
+{
+	// 1. N을 입력받는다.
+	scanf("%d", &N);
+
+	// 2. 단지의 모습을 입력 받는다.
+	for (int r = 0; r < N; ++r)
+	{
+		for (int c = 0; c < N; ++c)
+		{
+			scanf("%1d", &map[r][c]);
+		}
+	}
+
+	// 3. 단지 수와 단지 내 집의 개수를 구한다.
+	int complexCount = 0;
+	vector<int> houseCounts;
+	for (int r = 0; r < N; ++r)
+	{
+		for (int c = 0; c < N; ++c)
+		{
+			// 집을 찾았고 ,방문하지 않았다면?
+			if (map[r][c] == 1 && isVisited[r][c] == false)
+			{
+				int houseCount = dfs(r, c);
+				houseCounts.push_back(houseCount);
+				++complexCount;
+			}
+		}
+	}
+	sort(houseCounts.begin(), houseCounts.end());
+
+	// 4. 출력한다.
+	printf("%d\n", complexCount);
+	for (int houseCount : houseCounts)
+	{
+		printf("%d\n", houseCount);
+	}
+}
---
+++ 0629_혁진/백준_트리부모찾기/백준_트리부모찾기.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+int parr[100001];
+vector<int> tree[100001];
+bool isVisited[100001] = { false };
+
+void dfs(int loot)
+{
+	// 루트가 되면 트루로 바꾼다.
+	isVisited[loot] = true;
+	// 트리 사이즈만큼 반복
+	for (int i = 0;i < tree[loot].size();i++)
+	{
+		// 다음 방문할 곳은 tree[loot][i]
+		int next = tree[loot][i];
+		// 만약에 아직 안 간 곳이면
+		if (isVisited[next] == false)
+		{
+			// arr[k][i] = 루트 값이 됨
+			parr[next] = loot;
+			dfs(next);
+			// 후위 순회 보기cout << next << " ";
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> N;
+	// 트리에 값을 입력 받는다.
+	for (int i = 1; i < N; ++i)
+	{
+		int x, y;
+		cin >> x >> y;
+		tree[x].push_back(y);
+		tree[y].push_back(x);
+	}
+	//cout << "후위 순회 : ";
+	dfs(1);
+	//cout << "\n";
+	for (int i = 2; i <= N; ++i)
+	{
+		cout << parr[i] << "\n";
+	}
+}
