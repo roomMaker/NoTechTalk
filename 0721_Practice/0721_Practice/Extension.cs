482e3c876fe1366b4d8d2916ee37c24c21abf6cf
roomMaker <leehuk0622@gmail.com>
2022-07-21 17:24:55 +0900

    0721 정리

---
+++ 0721_Practice/0721_Practice/Extension.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+public static class Extension
+{
+    public static void SetIKPositionAndWeight(this Animator animator, AvatarIKGoal goal, Vector3 goalPosition, float weight = 1f)
+    {
+        animator.SetIKPositionAndWeight(goal, goalPosition);
+        animator.SetIKPosition(goal, goalPosition);
+    }
+}
---
+++ 0721_Practice/0721_Practice/Program.cs
@@ -0,0 +1,47 @@
+﻿//IEnumerator<int> TestIteratorMethod()
+//{
+//    yield return 1;
+//    yield return 2;
+//    yield return 3;
+//    yield return 4;
+//    yield return 5;
+//    yield return 6;
+//    yield return 7;
+//    yield return 8;
+//    yield return 9;
+//    yield return 10;
+//}
+
+//IEnumerator<int> enumerator = TestIteratorMethod();
+
+//while(enumerator.MoveNext())
+//{
+//    Console.WriteLine(enumerator.Current);
+//}
+
+class Base
+{
+    public virtual void Foo() { Console.WriteLine("Base의 Foo"); }
+}
+
+
+class Derived : Base
+{
+    public override void Foo()
+    {
+        base.Foo();
+    }
+}
+
+abstract class AbstractBase
+{
+    public abstract void Foo();
+}
+
+class Derived2 : AbstractBase
+{
+    public override void Foo()
+    {
+        
+    }
+}
